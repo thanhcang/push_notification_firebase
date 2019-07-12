0efc6cce9a90db97a7cf0726e55c30c6dc9e7a2d
Cang Vo <vtcanglt@gmail.com>
2019-07-12 09:48:34 +0700

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,57 @@
-const express = require("express");
-const cors = require("cors");
+const express    = require("express");
+const cors       = require("cors");
 const bodyParser = require("body-parser");
+const webpush = require('web-push')
+//svE1mZfISzUGe8b5Mm7rcNCDTCixxImbcWH3yBH0SCE
 
+//feVGGKFbWoI:APA91bGJ-XMvA6BOpfl06K9o4oLPZEBJcDNMeaBregQ4lgD4pFDgp5vLmbsa0jaGP2b9FQutwYVkq22SPO971U3Fote6QpHIzVWPs7J02iuHb9YiXsAwPbmrXEFrpECt7yn7PVdM7Dj4
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const port = 4000;
+const vapidKeys = {
+    publicKey:
+        'BDcEZIZj3WnoBxVGqL713C7XauAj-vXuFfGvruKJlS8vfK5afi-ZaWokNHghVc1cADMkhOX9ZPTSs5yiEmKBsMI',
+    privateKey: 'svE1mZfISzUGe8b5Mm7rcNCDTCixxImbcWH3yBH0SCE',
+};
+
+webpush.setVapidDetails(
+    'mailto:myuserid@email.com',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+)
+
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-const dummyDb = { subscription: null }; //dummy in memory store
+const dummyDb = {subscription : null}; //dummy in memory store
 
 const saveToDatabase = async subscription => {
-  // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
-  // Here you should be writing your db logic to save it.
-  dummyDb.subscription = subscription;
+    // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
+    // Here you should be writing your db logic to save it.
+    dummyDb.subscription = subscription;
 };
 
 // The new /save-subscription endpoint
 app.post("/save-subscription", async (req, res) => {
-  const subscription = req.body;
-  await saveToDatabase(subscription); //Method to save the subscription to Database
-  res.json({ message: "success" });
+    const subscription = req.body;
+    console.log(req.body);
+    await saveToDatabase(subscription); //Method to save the subscription to Database
+    res.json({message : "success"});
+});
+
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+    webpush.sendNotification(subscription, dataToSend)
+}
+//route to test send notification
+app.get('/send-notification', (req, res) => {
+    console.log(dummyDb.subscription)
+    const subscription = dummyDb.subscription //get subscription from your databse here.
+    const message = 'Hello World'
+    sendNotification(subscription, message)
+    res.json({ message: 'message sent' })
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- frontend/service.js
+++ frontend/service.js
@@ -1,49 +1,77 @@
 // urlB64ToUint8Array is a magic function that will encode the base64 public key
 // to Array buffer which is needed by the subscription option
+console.log('Hello from service worker')
+
 const urlB64ToUint8Array = base64String => {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
+    const padding     = "=".repeat((4 - (base64String.length % 4)) % 4);
+    const base64      = (base64String + padding)
+        .replace(/\-/g, "+")
+        .replace(/_/g, "/");
+    const rawData     = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
 };
 
 const saveSubscription = async subscription => {
-  const SERVER_URL = "http://localhost:4000/save-subscription";
-  const response = await fetch(SERVER_URL, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(subscription)
-  });
-  return response.json();
+    const SERVER_URL = "http://localhost:4000/save-subscription";
+    const response   = await fetch(SERVER_URL, {
+        method  : "post",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body    : JSON.stringify(subscription)
+    });
+    return response.json();
 };
 
+self.addEventListener('activate', async () => {
+    // This will be called only once when the service worker is activated.
+    try {
+        const applicationServerKey = urlB64ToUint8Array(
+            'BDcEZIZj3WnoBxVGqL713C7XauAj-vXuFfGvruKJlS8vfK5afi-ZaWokNHghVc1cADMkhOX9ZPTSs5yiEmKBsMI'
+        )
+        const options              = {applicationServerKey, userVisibleOnly : true}
+        const subscription         = await self.registration.pushManager.subscribe(options)
+        const response             = await saveSubscription(subscription)
+        console.log(response)
+    } catch (err) {
+        console.log('Error', err)
+    }
+})
+
 self.addEventListener("install", async () => {
-  // This will be called only once when the service worker is installed for first time.
-  try {
-    const applicationServerKey = urlB64ToUint8Array(
-      "BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk"
-    );
-    const options = { applicationServerKey, userVisibleOnly: true };
-    const subscription = await self.registration.pushManager.subscribe(options);
-    const response = await saveSubscription(subscription);
-    console.log(response);
-  } catch (err) {
-    console.log("Error", err);
-  }
+    // This will be called only once when the service worker is installed for first time.
+    try {
+        const applicationServerKey = urlB64ToUint8Array(
+            "BDcEZIZj3WnoBxVGqL713C7XauAj-vXuFfGvruKJlS8vfK5afi-ZaWokNHghVc1cADMkhOX9ZPTSs5yiEmKBsMI"
+        );
+        const options              = {applicationServerKey, userVisibleOnly : true};
+        const subscription         = await self.registration.pushManager.subscribe(options);
+        const response             = await saveSubscription(subscription);
+        console.log(response);
+    } catch (err) {
+        console.log("Error", err);
+    }
 });
 
-self.addEventListener("push", function(event) {
-  if (event.data) {
-    console.log("Push event!! ", event.data.text());
-  } else {
-    console.log("Push event but no data");
-  }
+self.addEventListener("push", function (event) {
+    if (event.data) {
+        showLocalNotification("Yolo", event.data.text(), self.registration);
+        console.log("Push event!! ", event.data.text());
+    } else {
+        console.log("Push event but no data");
+    }
 });
+
+const showLocalNotification = (title, body, swRegistration) => {
+    const options = {
+        body,
+        // here you can add more properties like icon, image, vibrate, etc.
+    };
+    swRegistration.showNotification(title, options);
+
+}
+
